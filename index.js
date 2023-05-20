99dc619a9e06233298fa4e9207a4176e17fb614d
root <gangadharr18@gmail.com>
2023-05-20 08:25:30 +0000

    coding-practice-1-congratscardapp

---
+++ index.js
@@ -0,0 +1,26 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card-container">
+      <div>
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        />
+      </div>
+      <h1 className="name">Kiran V</h1>
+      <p className="para">
+        Vishnu institute of computer education and technology,Bhimavaram.
+      </p>
+      <div>
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        />
+      </div>
+    </div>
+  </div>
+  // Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
